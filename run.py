645accb88a0b521aa0d1a938d453c230722af7bf
Angora King <angoraking@users.noreply.github.com>
2025-07-12 14:36:07 -0400

    c

--- run.py
+++ run.py
@@ -27,10 +27,12 @@ bsm_dev = BotoSesManager(
 )
 with bsm_dev.awscli():
     project.bsm.print_who_am_i(masked=True)
-project.crawl_all_video_details_in_one_html_database(
+# project.crawl_all_video_details_in_one_html_database(
+#     lang_code=LangCodeEnum.cn.value,
+#     node_id=0,
+#     big_batch_size=10,
+#     micro_batch_size=3,
+# )
+project.step_04_01_crawl_all_video_details_in_all_html_database(
     lang_code=LangCodeEnum.cn.value,
-    node_id=0,
-    big_batch_size=10,
-    micro_batch_size=3,
 )
-# project.step_04_01_crawl_all_video_details_in_all_html_database(lang_code=LangCodeEnum.cn.value)
